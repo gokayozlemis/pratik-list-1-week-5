25674c7ddbd37743cfa7c7f0117e7ffadd90bdba
gokay <gokay@GOKAY>
2025-01-16 19:15:11 +0300

    Add project files.

---
+++ pratik list 1 week 5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("** DAVETLİLERRR **");
+
+        List<string> ziyaretciListesi = new List<string> { "Bülent ERSOY", "Ajda PEKKAN", "Ebru GÜNDEŞ", "Hadise", "Hande YENER","Tarkan","Funda ARAR","Demet AKALIN"};
+
+        foreach (string isim in ziyaretciListesi)
+        {
+            Console.WriteLine(isim);
+        }
+
+        for (int i = 0; i < ziyaretciListesi.Count; i++)
+        {
+            Console.WriteLine(ziyaretciListesi[i]);
+        }
+    }
+}
